4111516001dd8623220a80d06c90829aecaf152f
Vlad Phokhnitskyi <66656318+Nansokolov@users.noreply.github.com>
2024-05-27 12:17:33 -0400

    Cart design completed

--- carStoreManager/Models/CartItem.cs
+++ carStoreManager/Models/CartItem.cs
@@ -8,5 +8,6 @@
         public string? Price { get; set; }
         public int Quantity { get; set; }
         public string UserId { get; set; } // Store the user's ID
+        public Car Car { get; set; }
     }
 }
